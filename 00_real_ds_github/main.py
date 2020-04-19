d171ebfef6c11dd1056909ca5617e6748e78553f
Regina Galieva <rg.galieva@gmail.com>
2020-04-19 00:33:22 -0700

    Project 0. GitHub. Quick start - Extend docs

--- 00_real_ds_github/main.py
+++ 00_real_ds_github/main.py
@@ -1,6 +1,19 @@
 import numpy as np
 
-# VERSION ONE
+# Game
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+
+# Example from the task: version one
 def game_core_v1(number):
     '''Просто угадываем на random, никак не используя информацию о больше или меньше.
        Функция принимает загаданное число и возвращает число попыток'''
@@ -12,7 +25,7 @@ def game_core_v1(number):
             return(count) # выход из цикла, если угадали
 
 
-# VERSION TWO
+# Example from the task: version two
 def game_core_v2(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
@@ -27,41 +40,37 @@ def game_core_v2(number):
     return(count) # выход из цикла, если угадали
 
 
-# FINAL VERSION: Binary search
-def game_core(randomNumber, predictionList = range(1, 101)):
-    counter = 0
-    first = predictionList[0]
-    last = predictionList[-1]
+# Solution
+def game_core(targetValue, predictionList=range(1, 101)):
+    '''Find targetValue in a given predictionList with minimum number of attempts.
+
+        Divide and conquer by using binary search algorithm:
+        - Create sorted predictionList with numbers from 1 to 100
+        - Split predictionList into two halves
+        - Compare the target value to the middle element of the predictionList
+            If they are not equal, the half in which the target cannot lie is eliminated 
+            and the search continues on the remaining half
+        - After each iteration narrow down the predictionList by excluding already checked values
+        - Repeat until the target value is found.
+
+        Returns the number of iterations required to find targetValue.
+    '''
+    count = 0
+    left_edge = predictionList[0]
+    right_edge = predictionList[-1]
     
-    while first <= last:
-        counter += 1
-        midpoint = (first + last) // 2
+    while left_edge <= right_edge:
+        count += 1
+        midpoint = (left_edge+right_edge) // 2
 
-        # Check if randomNumber is present at mid 
-        if randomNumber == predictionList[midpoint]:        
-            break 
-        # If randomNumber is greater than mid value, ignore left half 
-        elif randomNumber > predictionList[midpoint]:
-            first = midpoint + 1
-        # If randomNumber is greater, ignore right half 
+        # Check if targetValue is present at mid 
+        if targetValue == predictionList[midpoint]:        
+            break
+        # If targetValue is greater than mid value, ignore left half 
+        elif targetValue > predictionList[midpoint]:
+            left_edge = midpoint + 1
+        # If targetValue is greater, ignore right half 
         else:
-            last = midpoint - 1
+            right_edge = midpoint - 1
 
-    return counter
-
-# GAME
-def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
-    count_ls = []
-    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1,101, size=(1000))
-    for number in random_array:
-        count_ls.append(game_core(number))
-    score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return(score)
-
-# GAME TEST
-score_game(game_core_v1) # => 101
-score_game(game_core_v2) # => 33
-score_game(game_core) # => 5
+    return count
